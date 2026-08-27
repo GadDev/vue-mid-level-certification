e72bd3aec5ea18cb34fc286612d09d5007cc4940
GadDev <alex.gadaix@gmail.com>
2026-08-27 09:47:43 +0200

    feat: specify item structure in DataPanel props for improved type safety

--- solutions/31-panel-forwarding/src/components/PanelFrame.vue
+++ solutions/31-panel-forwarding/src/components/PanelFrame.vue
@@ -12,7 +12,7 @@ defineProps<{
   <section data-testid="panel-frame">
     <h2 data-testid="panel-frame-title">{{ title }}</h2>
 
-    <DataPanel v-bind="$attrs">
+    <DataPanel v-bind="$attrs as { items: { id: string | number }[] }">
       <template v-for="(_, name) in $slots" :key="name" #[name]="slotProps">
         <slot :name="name" v-bind="slotProps ?? {}" />
       </template>
